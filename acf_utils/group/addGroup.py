18839b4f9ac3411bdb51409c4d1b73b74b6774cd
serii <seriiburduja@gmail.com>
2024-04-04 15:39:28 +0300

    feat: addGroup

--- acf_utils/group/addGroup.py
+++ acf_utils/group/addGroup.py
@@ -18,22 +18,22 @@ def addGroup(file_path, group_id = None):
             data = json.load(file)
             if group_id:
                 group_path = getGroupPathById(file_path, group_id)
-                print(f"group_path: {group_path}")
                 new_tab = newTab(new_group_name)
                 new_group = newGroup(new_group_name)
                 exec(f"{group_path}['sub_fields'].append({new_tab})")
                 exec(f"{group_path}['sub_fields'].append({new_group})")
                 newData = json.dumps(data, indent=4)
-                print("Group added successfully!")
+                # print("Group added successfully!")
             else:
                 new_tab = newTab(new_group_name)
                 data[0]['fields'].append(new_tab)
                 new_group = newGroup(new_group_name)
                 data[0]['fields'].append(new_group)
                 newData = json.dumps(data, indent=4)
-                print(json.dumps(data[0]['fields'], indent=4))
+                # print(json.dumps(data[0]['fields'], indent=4))
 
         with open(file_path, 'w') as file:
             # write
             file.write(newData)
+            print(colored("Group added successfully!", "green"))
         #
