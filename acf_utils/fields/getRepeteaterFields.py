8edd8caf996c5f11de654013289e9fded56f8874
serii <seriiburduja@gmail.com>
2024-04-05 10:19:00 +0300

    feat: error if no repeater

--- acf_utils/fields/getRepeteaterFields.py
+++ acf_utils/fields/getRepeteaterFields.py
@@ -1,16 +1,15 @@
 import json
 
-def getRepeaterFields(file_path):
+def getRepeaterFields(file_path, group_index):
     repeater_fields = []
     f = open(file_path,)
     data = json.load(f)
-    for i in data:
-        for j in i['fields']:
-            if j['type'] == "group":
-                sub_fields = j['sub_fields']
-                for k in sub_fields:
-                    if k['type'] == "repeater":
-                        repeater_field = {'name': k['name'], 'label': k['label'], 'index': sub_fields.index(k)}
-                        repeater_fields.append(repeater_field)
+    sub_fields = data[0]['fields'][group_index]['sub_fields']
+    for k in sub_fields:
+        if k['type'] == "repeater":
+            repeater_field = {'name': k['name'], 'label': k['label'], 'index': sub_fields.index(k)}
+            repeater_fields.append(repeater_field)
+        else:
+            continue
     return repeater_fields
 
--- acf_utils/group/repeaterFieldsMenu.py
+++ acf_utils/group/repeaterFieldsMenu.py
@@ -1,45 +1,36 @@
 from termcolor import colored
 from acf_utils.fields.addField import addField
-from acf_utils.fields.chooseRepeaterField import chooseRepeaterField
 from acf_utils.fields.deleteField import deleteField
 from acf_utils.fields.editField import editField
-from acf_utils.group.repeaterFieldsMenu import repeaterFieldsMenu
 
 from acf_utils.group.showAll import showAll
 from acf_utils.wp.wpExport import wpExport
 from acf_utils.wp.wpImport import wpImport
 
 
-def groupMenu(file_path, group_index):
+def repeaterFieldsMenu(file_path, group_index, field_index):
     print('----------------------------- Group Menu -----------------------------')
     print(colored("1) Show All:", "yellow"))
     print(colored("2) Add Field:", "blue"))
-    print(colored("3) Choose Repeater Field:", "green"))
-    print(colored("4) Edit Field:", "blue"))
-    print(colored("5) Delete Field:", "blue"))
-    print(colored("6) Import:", "red"))
-    print(colored("7) Export:", "red"))
-    print(colored("8) Exit", "red"))
+    print(colored("3) Edit Field:", "blue"))
+    print(colored("4) Delete Field:", "blue"))
+    print(colored("5) Import:", "red"))
+    print(colored("6) Export:", "red"))
+    print(colored("7) Exit", "red"))
     showAll(file_path, group_index)
     action = input("Enter your choice: ")
     if action == "1":
         showAll(file_path, group_index)
-        groupMenu(file_path, group_index)
+        repeaterFieldsMenu(file_path, group_index, field_index)
     if action == "2":
-        addField(file_path, group_index)
-        groupMenu(file_path, group_index)
-    if action == "3":
-        field_index = chooseRepeaterField(file_path, group_index)
-        if field_index == False:
-            groupMenu(file_path, group_index)
-        else:
-            repeaterFieldsMenu(file_path, group_index, field_index)
+        addField(file_path, group_index, field_index)
+        repeaterFieldsMenu(file_path, group_index, field_index)
     if action == "4":
         editField(file_path, group_index)
-        groupMenu(file_path, group_index)
+        repeaterFieldsMenu(file_path, group_index, field_index)
     if action == "5":
         deleteField(file_path, group_index)
-        groupMenu(file_path, group_index)
+        repeaterFieldsMenu(file_path, group_index, field_index)
     elif action == "6":
         wpImport()
         exit()
@@ -49,4 +40,4 @@ def groupMenu(file_path, group_index):
     elif action == "8":
         exit()
     else:
-        groupMenu(file_path, group_index)
+        repeaterFieldsMenu(file_path, group_index, field_index)
